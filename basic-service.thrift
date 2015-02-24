883dea9c69e5d70b5bd3edc9b29f24b42182b939
Rob Cameron <rwcameron@gmail.com>
2015-02-24 00:03:19 -0800

    fun town

---
+++ basic-service.thrift
@@ -0,0 +1,12 @@
+namespace go basicservice
+namespace py basicservice
+namespace rb basicservice
+
+typedef i64 bigTime
+//KeyValue service
+service KeyValue 
+{
+    //Ping to check if server is alive
+    void ping(),
+    void setValue(1:i32 key, 2:string value),
+}
