17fc589202973ca12b1844e9fa3cf1e4ea5471f0
Rob Cameron <rwcameron@gmail.com>
2015-02-27 16:49:34 -0800

    adding data

--- basic-service.thrift
+++ basic-service.thrift
@@ -4,9 +4,11 @@ namespace rb basicservice
 
 typedef i64 bigTime
 //KeyValue service
-service KeyValue 
+service KeyValue
 {
-    //Ping to check if server is alive
+    /*
+     * Ping to check if server is alive
+     */
     void ping(),
     void setValue(1:i32 key, 2:string value),
 }
